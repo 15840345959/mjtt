11202c1306c93e371946759617dba81d2d1e32d5
ZhangMeng666666 <zm_happiness66@126.com>
2017-12-29 15:32:44 +0800

    by zm 主播详情——最新节目表页

--- script/common.js
+++ script/common.js
@@ -6,3 +6,30 @@ function download(){
         $(location).attr('href','http://a.app.qq.com/o/simple.jsp?pkgname=com.tommy.mjtt_an_pro&channel=0002160650432d595942&fromcase=60001')
     }
 }
+
+
+function playMusic(id){
+    var player = $("#player")[0]; /*jquery对象转换成js对象*/
+    var player_src=$("#player").attr("src")  //正在播放的音频文件
+    var audio_index=$("#program_play_"+id).attr("audio-index");  //要播放的音频文件
+    console.log("正在播放的音频文件 is : " + JSON.stringify(player_src)+" ; 要播放的音频文件 is : " + JSON.stringify(audio_index))
+    if(player_src==audio_index){
+        if (player.paused){ /*如果已经暂停*/
+            player.play(); /*播放*/
+            $("#program_play_"+id+" .style-program-list-cover-play img").attr("src","./image/play_close.png")
+        }else {
+            player.pause();/*暂停*/
+            $("#program_play_"+id+" .style-program-list-cover-play img").attr("src","./image/play_start.png")
+        }
+    }
+    else{
+        player.pause();/*暂停*/
+        $("#player").attr("src",audio_index);
+        var player = $("#player")[0]; /*jquery对象转换成js对象*/
+        var player_src2=$("#player").attr("src")
+        console.log("要更换的音频文件 is : " + JSON.stringify(player_src2))
+        player.play(); /*播放*/
+        $(".style-program-list-cover-play img").attr("src","./image/play_start.png")
+        $("#program_play_"+id+" .style-program-list-cover-play img").attr("src","./image/play_close.png")
+    }
+}
